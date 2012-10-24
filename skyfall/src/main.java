5019599e0854f21d644346ef4655e43f009403a7
Hristiqn Slavov <mr__die@abv.bg>
2012-10-24 12:13:27 +0300

    skyfall

---
+++ skyfall/src/main.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class main
+{
+
+    public static void main(String[] args)
+    {
+        readFile("text.txt");
+    }
+
+    public static void readFile(String file)
+    {
+        
+    }
+
+}
